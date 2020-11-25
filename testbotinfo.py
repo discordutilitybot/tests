828d26e53cc0af2552a0d0e9600940c45a830110
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2020-11-24 20:58:09 -0500

    Create testbotinfo.py

---
+++ testbotinfo.py
@@ -0,0 +1,20 @@
+import unittest
+import unittest.mock
+import discord
+from discord.ext import commands
+import command
+from .command import botinfo
+
+class TestBotInfo(unittest.TestCase):
+
+    async def setUp(self):
+        self.app = botinfo.app.setup()
+        self.app.testing = True
+    
+    async def test_bot_info(self):
+        logmsg = "Testing bot info."
+        self.assertEqual(self.app.botinfo(), logmsg)
+ 
+
+if __name__ == "__main__":
+    unittest.main()
